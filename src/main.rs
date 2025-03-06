ea3fb6be58aa8b35e4981381a69d5d0110404f71
JustaPuffin <falkbrand01@gmail.com>
2025-03-06 15:28:12 +0100

    feat(#1): functional roster

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use macroquad::prelude::*;
 
-const BACKGROUND: Color = BLACK;
-const EMPTY: Color = WHITE;
-const FILLED: Color = BLUE;
 
+type ModeType: = i16;
+
+
+const BACKGROUND: Color = BLACK; // Background colour
+const EMPTY: Color = WHITE; // Empty Square
+const FILLED: Color = BLUE; // Filled Square
+const CROSSED: Color = GRAY; // Crossed out Square
+
+
+
+// Used for squares in the nonogram roster
 struct Field {
     x: f32,
     y: f32,
+    w: f32,
+    h: f32,
     size: f32,
     colour: Color,
     filled: bool,
@@ -15,35 +25,40 @@ struct Field {
 
 #[macroquad::main("Nonogram")]
 async fn main() {
-    let mut roster: Vec<Vec<Field>> = get_roster(5);
+    let mut roster= get_roster(25, 20, 20);
     loop {
         clear_background(BACKGROUND);
+
+        if is_mouse_button_pressed(MouseButton::Left) {roster = check_fields(MouseButton::Left, roster)}
+        else if is_mouse_button_pressed(MouseButton::Right) {roster = check_fields(MouseButton::Right, roster)}
+
         draw_roster(&roster);
         next_frame().await
     }
 }
 
 // gets a clear roster
-fn get_roster(size: usize) -> Vec<Vec<Field>> {
+fn get_roster(width: usize, height: usize, size: usize)  -> Vec<Vec<Field>> {
     let mut roster: Vec<Vec<Field>> = vec![];
-
-    for y in 0..size {
+    for y in 0..height {
         roster.push(vec![]);
-        for x in 0..size {
+        for x in 0..width {
             roster[y].push(
                 Field {
                     x: x as f32 * 20.0 + (x as f32 * 2.0),
                     y: y as f32 * 20.0 + (y as f32 * 2.0),
+                    w: size as f32,
+                    h: size as f32,
                     size: 20.0,
                     colour: EMPTY,
                     filled: false,
                     crossed: false
                 }
-            );
+            )
         }
         
     }
-    
+
     return roster;
 }
 
@@ -54,10 +69,42 @@ fn draw_roster(roster: &Vec<Vec<Field>>) {
             draw_rectangle(
                 roster[y][x].x,
                 roster[y][x].y,
-                roster[y][x].size,
-                roster[y][x].size,
+                roster[y][x].w,
+                roster[y][x].h,
                 roster[y][x].colour,
             );
         }
     }
+}
+
+fn check_fields(input: MouseButton, roster: Vec<Vec<Field>>) -> Vec<Vec<Field>> {
+    let mut temp: Vec<Vec<Field>> = roster;
+    for y in 0..temp.len() {
+        for x in 0..temp[y].len() {
+            if mouse_position().0 > temp[y][x].x && mouse_position().0 < temp[y][x].x + temp[y][x].size {
+                if mouse_position().1 > temp[y][x].y && mouse_position().1 < temp[y][x].y + temp[y][x].size {
+                    match input {
+                        MouseButton::Left => if temp[y][x].filled {
+                            temp[y][x].colour = EMPTY;
+                            temp[y][x].filled = false;
+                        } else {
+                            temp[y][x].colour = FILLED;
+                            temp[y][x].filled = true;
+                            temp[y][x].crossed = false;
+                        }
+                        MouseButton::Right => if temp[y][x].crossed {
+                            temp[y][x].colour = EMPTY;
+                            temp[y][x].crossed = false;
+                        } else {
+                            temp[y][x].colour = CROSSED;
+                            temp[y][x].filled = false;
+                            temp[y][x].crossed = true;
+                        }
+                        _ => continue
+                    }
+                }
+            }
+        }
+    }
+    return temp;
 }
