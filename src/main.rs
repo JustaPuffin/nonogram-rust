dfba340888273ac0efa6b58404c6be6ea382c1b0
JustaPuffin <falkbrand01@gmail.com>
2025-03-06 12:24:40 +0100

    feat(#1): added all rectangle parameters to struct

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ const EMPTY: Color = WHITE;
 const FILLED: Color = BLUE;
 
 struct Field {
+    x: f32,
+    y: f32,
+    size: f32,
     colour: Color,
     filled: bool,
     crossed: bool,
@@ -12,22 +15,26 @@ struct Field {
 
 #[macroquad::main("Nonogram")]
 async fn main() {
-    
+    let mut roster: Vec<Vec<Field>> = get_roster(5);
     loop {
         clear_background(BACKGROUND);
-        draw_fields(get_fields(5));
+        draw_roster(&roster);
         next_frame().await
     }
 }
 
-fn get_fields(size: usize) -> Vec<Vec<Field>> {
+// gets a clear roster
+fn get_roster(size: usize) -> Vec<Vec<Field>> {
     let mut roster: Vec<Vec<Field>> = vec![];
 
     for y in 0..size {
         roster.push(vec![]);
-        for _x in 0..size {
+        for x in 0..size {
             roster[y].push(
                 Field {
+                    x: x as f32 * 20.0 + (x as f32 * 2.0),
+                    y: y as f32 * 20.0 + (y as f32 * 2.0),
+                    size: 20.0,
                     colour: EMPTY,
                     filled: false,
                     crossed: false
@@ -40,13 +47,15 @@ fn get_fields(size: usize) -> Vec<Vec<Field>> {
     return roster;
 }
 
-fn draw_fields(roster: Vec<Vec<Field>>) {
+// draws a square matrix
+fn draw_roster(roster: &Vec<Vec<Field>>) {
     for y in 0..roster.len() {
         for x in 0..roster[y].len() {
             draw_rectangle(
-                x as f32 * 20.0 + (x as f32 * 2.0),
-                y as f32 * 20.0 + (y as f32 * 2.0),
-                20.0, 20.0,
+                roster[y][x].x,
+                roster[y][x].y,
+                roster[y][x].size,
+                roster[y][x].size,
                 roster[y][x].colour,
             );
         }
